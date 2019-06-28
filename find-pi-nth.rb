f8a8bb5e51a03773b65a7b29637767e8d80a172e
sampraslee <sampras.lee8@gmail.com>
2019-06-28 10:17:05 +0800

    Used  .truncate method to limit PI.

--- find-pi-nth.rb
+++ find-pi-nth.rb
@@ -1,2 +1,10 @@
 # Storing Pi in a variable
 PI = Math::PI
+
+puts "Hello there, I can help you find the value of PI to your preferred decimal place."
+puts "Please type you desired decimal place..."
+
+decimal_place = gets.chomp.to_i
+
+puts "PI to the #{decimal_place}th decimal place is...."
+puts "#{PI.truncate(decimal_place)}"
